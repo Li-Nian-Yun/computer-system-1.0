75dfcbc8ddfad86e8ab7728dc5ca347f6efd7b13
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 11:36:24 +0000

    >  compile NEMU 3019244406 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:36:24 up 5 min,  1 user,  load average: 0.45, 0.54, 0.28 cef66c68d049b6c32c83b29cf9f8ca6564a6f046

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -46,12 +46,14 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+       
 	/* TODO: Add more commands */
 
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
+
+
 
 static int cmd_help(char *args) {
 	/* extract the first argument */
